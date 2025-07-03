5e08967f105ebf9ea3e3f0cd7b88ed0054b42178
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2025-07-03 11:52:06 +0800

    Implement Division model, migrations, and seeder; integrate Spatie permission package

--- database/migrations/0001_01_01_000001_create_users_table.php
+++ database/migrations/0001_01_01_000001_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Division;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,9 +15,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nip')->nullable()->unique();
+            $table->string("position")->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignIdFor(Division::class)->nullable()->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Division;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seeder Divisi
+        $divisions = ["Manajemen", "Office Boy", "Laboratorium", "IT", "Keuangan"];
+        foreach($divisions as $division){
+            Division::create([
+                "name" => $division
+            ]);
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Seeder Role
+        foreach(["Superadmin", "Admin", "Admin Divisi", "Admin Gudang", "Pimpinan"] as $role){
+            Role::create(["name" => $role]);
+
+            if($role != "Admin Divisi"){
+                User::create([
+                    "name" => str_replace(" ", "", strtolower($role)),
+                    "email" => str_replace(" ", "", strtolower($role))."@gmail.com",
+                    "password" => bcrypt("password"),
+                    "position" => $role
+                ])->assignRole($role);
+            }
+        }
+
+        foreach($divisions as $division){
+            User::create([
+                "name" => "admin" . str_replace(" ", "", strtolower($division)),
+                "email" => "admin" . str_replace(" ", "", strtolower($division))."@gmail.com",
+                "password" => bcrypt("password"),
+                "position" => "Admin Divisi " . $division
+            ])->assignRole("Admin Divisi");
+        }
     }
 }
